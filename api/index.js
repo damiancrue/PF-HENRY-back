1d5bb4dd53597e82397130cbaa1c86b39f8ef52d
mmachu <matiasmachulini@gmail.com>
2022-09-04 01:36:02 -0300

    Creates schedules CRUD

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js");
 const loadData = require("./src/helpers/loadData.js"); //Agrega los roles cada vez que se inicializa la base de datos
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  loadData(); //Ejecuta para agregar los roles a la DB
+  //loadData(); //Ejecuta para agregar los roles a la DB
   server.listen(process.env.PORT || 3001, () => {
     console.log("listening at port " + process.env.PORT); // eslint-disable-line no-console
   });
